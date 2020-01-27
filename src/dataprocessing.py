d85cdff653bf1847bf345035aafd6dc439a2f0b7
Mike Chen <mike_T_Chen@hotmail.com>
2020-01-26 18:56:33 -0800

    Updated data wrangling script to fix issue regarding different number of columns btwn test and train dataset

--- src/dataprocessing.py
+++ src/dataprocessing.py
@@ -17,7 +17,8 @@ def wrangler(input_path, output_path):
     y = df['DEPARTURE_DELAY']
 
     # First drop all the irrelevant files that won't be used in our analysis before splitting
-    X_relevant = X[['MONTH', 'AIRLINE', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DISTANCE', 'DAY_OF_WEEK']]
+    #X_relevant = X[['MONTH', 'AIRLINE', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DISTANCE', 'DAY_OF_WEEK']]
+    X_relevant = X[['MONTH', 'AIRLINE', 'DISTANCE', 'DAY_OF_WEEK']]
     y_numpy = np.array(y)
 
 
@@ -25,7 +26,8 @@ def wrangler(input_path, output_path):
     # indicate late flight
     y = np.where(y_numpy > 5.0, 1, 0).tolist()
 
-
+    # Turn y back into a dataframe
+    y = pd.DataFrame(y, columns = ["Target"])
 
     # Split the dataset into training and testing datasets 
     X_train, X_test, y_train, y_test = train_test_split(X_relevant,
@@ -42,23 +44,46 @@ def wrangler(input_path, output_path):
 
 
 
-    ############ THE DATA BASE IS TOOOOOOOOOOOOOOOOOOOOOO BIGGGGGG I CAN'T RUN IT WOULD HAVE 3.5 MIL ROW WITH 1.2K COL =O
-    X_train_small = X_train.head(50000)
-    X_test_small = X_test.head(50000)
-    y_train_small = y_train[:50000]
-    y_test_small = y_test[:50000]
+    # THE DATA BASE IS TOO BIG I CAN'T RUN IT WOULD HAVE 3.5 MIL ROW WITH 1.2K COL
+    # Work with a smaller dataset, because of computational and time restrictions 
+    X_train_small = X_train.head(8000)
+    X_test_small = X_test.head(8000)
+    y_train_small = y_train[:8000]
+    y_test_small = y_test[:8000]
+
+    numerics = ["DISTANCE"]
+    categoricals = ["MONTH", "AIRLINE", "DAY_OF_WEEK"]
+
+    # Column Transformer 
+    preprocessor = ColumnTransformer(
+        transformers = [
+            ('minmax', MinMaxScaler(), numerics),
+            ('ohe', OneHotEncoder(drop = "first", sparse = False, dtype = int), categoricals)
+        ]
+    )
+
+    # Clean X training dataset
+    X_train_clean = pd.DataFrame(preprocessor.fit_transform(X_train_small),
+            index = X_train_small.index,
+            columns = (numerics +
+                      list(preprocessor.named_transformers_['ohe']
+                          .get_feature_names(categoricals))))
+
+    # Clean X testing dataset
+    X_test_clean = pd.DataFrame(preprocessor.transform(X_test_small),
+                           index = X_test_small.index,
+                           columns = X_train_clean.columns)
 
 
+    # # Scale the distance column with a min max scaler 
+    # scaler = MinMaxScaler()
+    # X_train_small[['DISTANCE']] = scaler.fit_transform(X_train_small[['DISTANCE']])
+    # X_test_small[['DISTANCE']] = scaler.transform(X_test_small[['DISTANCE']])
 
-    # Scale the distance column with a min max scaler 
-    scaler = MinMaxScaler()
-    X_train_small[['DISTANCE']] = scaler.fit_transform(X_train_small[['DISTANCE']])
-    X_test_small[['DISTANCE']] = scaler.transform(X_test_small[['DISTANCE']])
 
-
-    # Get dummies columns for categorical columns 
-    X_train_temp = pd.get_dummies(X_train_small, prefix_sep='_', drop_first=True)
-    X_test_temp = pd.get_dummies(X_test_small, prefix_sep='_', drop_first=True)
+    # # Get dummies columns for categorical columns 
+    # X_train_temp = pd.get_dummies(X_train_small, prefix_sep='_', drop_first=True)
+    # X_test_temp = pd.get_dummies(X_test_small, prefix_sep='_', drop_first=True)
 
 
 
@@ -69,8 +94,8 @@ def wrangler(input_path, output_path):
 
     # SHOULD RETURN all the X_train, test y_train, test AND the original X_train without scaling/dummy variable 
     # as the EDA dataset so data exploration will be easier. 
-    X_train_temp.to_csv(output_path+"X_train_temp.csv")
-    X_test_temp.to_csv(output_path+"X_test_temp.csv")
+    X_train_clean.to_csv(output_path+"X_train_clean.csv")
+    X_test_clean.to_csv(output_path+"X_test_clean.csv")
     y_train_small.to_csv(output_path+"y_train.csv")
     y_test_small.to_csv(output_path+"y_test.csv")
 
@@ -78,7 +103,6 @@ def wrangler(input_path, output_path):
 
     X_relevant.to_csv(output_path+"X_original.csv")
     y.to_csv(output_path+"y_original.csv")
-
 
 
 wrangler("flights.csv", "data/")
